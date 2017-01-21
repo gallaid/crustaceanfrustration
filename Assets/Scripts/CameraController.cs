9d615674de9fd9a4ba10ee6058b87f14d238dbea
Gani Parrott <gparrott@ucsc.du>
2017-01-20 20:07:13 -0800

    character being controlled

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public Vector3 _startingRelativePosition = new Vector3(0, 4, -3);
+
+	public float rotationVelocity = 5f;
+
+	private GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+		playerObject = GameObject.FindGameObjectWithTag("Player");
+		transform.parent = playerObject.transform;
+		transform.position = _startingRelativePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleInput();
+	}
+
+	private void HandleInput() {
+		float horizontal = Input.GetAxis("HorizontalAxisRotation");
+		float vertical = Input.GetAxis("VerticalAxisRotation");
+		float mouseX = Input.GetAxis("Mouse X");
+		float mouseY = Input.GetAxis("Mouse Y");
+
+		if (horizontal < 0 || mouseY > 0) {
+			RotateDown();
+		}
+		else if (horizontal > 0 || mouseY < 0) {
+			RotateUp();
+		}
+
+		if (vertical < 0 || mouseX < 0) {
+			RotateLeft();
+		}
+		else if (vertical > 0 || mouseX > 0) {
+			RotateRight();
+		}
+	}
+
+	private void RotateLeft() {
+		playerObject.transform.Rotate(-playerObject.transform.up * rotationVelocity * Time.deltaTime);
+	}
+
+	private void RotateRight() {
+		playerObject.transform.Rotate(playerObject.transform.up * rotationVelocity * Time.deltaTime);
+	}
+
+	private void RotateUp() {
+		transform.RotateAround(playerObject.transform.position,
+							   playerObject.transform.right,
+							   Time.deltaTime * rotationVelocity);
+	}
+
+	private void RotateDown() {
+		transform.RotateAround(playerObject.transform.position,
+							   playerObject.transform.right,
+							   -Time.deltaTime * rotationVelocity);	
+	}
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float _velocity = 1;
+
+	private enum MoveDirection {
+		Forward,
+		Back,
+		Left,
+		Right
+	}
+
+	private Dictionary<MoveDirection, Action> moveMap = new Dictionary<MoveDirection, Action>();
+
+	// Use this for initialization
+	void Start () {
+		moveMap.Add(MoveDirection.Forward,
+		() => {
+			transform.position += (transform.forward * Time.deltaTime * _velocity);
+		});
+		
+		moveMap.Add(MoveDirection.Back,
+		() => {
+			transform.position += (-transform.forward * Time.deltaTime * _velocity);
+		});
+		
+		moveMap.Add(MoveDirection.Left,
+		() => {
+			transform.position += (-transform.right * Time.deltaTime * _velocity);
+		});
+		
+		moveMap.Add(MoveDirection.Right,
+		() => {
+			transform.position += (transform.right * Time.deltaTime * _velocity);
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HandleInput();	
+	}
+
+	private void HandleInput() {
+		var horizontal = Input.GetAxis("Horizontal");
+		var vertical = Input.GetAxis("Vertical");
+		if (horizontal > 0) {
+			moveMap[MoveDirection.Right]();
+		}
+		else if (horizontal < 0) {
+			moveMap[MoveDirection.Left]();
+		}
+
+		if (vertical > 0) {
+			moveMap[MoveDirection.Forward]();
+		}
+		else if (vertical < 0) {
+			moveMap[MoveDirection.Back]();
+		}
+	}
+}
