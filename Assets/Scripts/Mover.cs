7a498b69740a8ede2ce05a6d5a1cb59e602afb01
g-parrott <gparrottgames@gmail.com>
2017-01-21 16:39:27 -0800

    camera control is fixed. animation functioning

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -58,58 +58,58 @@ public class Mover : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Environment")
-        {
-            // get highest contact point
-            float max = collision.contacts[0].point.y;
-            foreach (var contact in collision.contacts)
-            {
-                if (contact.point.y > max)
-                {
-                    max = contact.point.y;
-                }
-            }
+        //if (collision.gameObject.tag == "Environment")
+        //{
+        //    // get highest contact point
+        //    float max = collision.contacts[0].point.y;
+        //    foreach (var contact in collision.contacts)
+        //    {
+        //        if (contact.point.y > max)
+        //        {
+        //            max = contact.point.y;
+        //        }
+        //    }
 
-            // set the position to the highest contact point with plane axes unchanged
-            transform.position = new Vector3(transform.position.x, max, transform.position.z);
-        }
+        //    // set the position to the highest contact point with plane axes unchanged
+        //    transform.position = new Vector3(transform.position.x, max, transform.position.z);
+        //}
     }
 
     private void HandleInput()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
+
+        bool isWalking = false;
+
         if (horizontal > 0)
         {
+            isWalking = true;
             moveMap[MoveDirection.Right]();
         }
         else if (horizontal < 0)
         {
+            isWalking = true;
             moveMap[MoveDirection.Left]();
         }
 
         if (vertical > 0)
         {
+            isWalking = true;
             moveMap[MoveDirection.Forward]();
         }
         else if (vertical < 0)
         {
+            isWalking = true;
             moveMap[MoveDirection.Back]();
         }
+
+        GetComponent<Animator>().SetBool("Walking", isWalking);
 
         // rotate the player according to camera direction
         Vector3 cameraDirection = Camera.main.transform.forward.normalized;
         Vector3 playerDirection = Vector3.ProjectOnPlane(cameraDirection, transform.up).normalized;
         float angleToRotate = Vector3.Angle(transform.forward, playerDirection);
-        Debug.Log(angleToRotate);
-        if (angleToRotate > 90)
-        {
-            Debug.Log("Rotating by" + (angleToRotate - 360));
-            transform.Rotate(transform.up, angleToRotate - 360, Space.World);
-        }
-        else
-        {
-            transform.Rotate(transform.up, angleToRotate, Space.World);
-        }
+        transform.LookAt(playerDirection + transform.position);
     }
 }
